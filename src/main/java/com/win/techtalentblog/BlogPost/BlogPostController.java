560a8f514444a46c5b856d92ce605fbe475472f6
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-06-23 10:36:57 -0400

    new.html update controller and reformatted index page

--- src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPostController.java
@@ -1,5 +1,8 @@
 package com.win.techtalentblog.BlogPost;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,14 +15,22 @@ public class BlogPostController {
     @Autowired
     private BlogPostRepository blogPostRepository;
 
-    @GetMapping
-    public String index(BlogPost blogPost) {
+    private static List<BlogPost> posts = new ArrayList<>();
+
+    @GetMapping(value = "/")
+    public String index(BlogPost blogPost, Model model) {
+        model.addAttribute("posts", posts);
     return "blogpost/index";
     }
 
     private BlogPost blogPost;
 
-    @PostMapping
+    @GetMapping(value = "/blogposts/new")
+    public String newBlog(BlogPost blogPost) {
+        return "blogpost/new";
+    }
+
+    @PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model){
         blogPostRepository.save(new BlogPost(blogPost.getTitle(), blogPost.getAuthor(), blogPost.getBlogEntry()));
         model.addAttribute("title", blogPost.getTitle());
